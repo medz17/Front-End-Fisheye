b9f11d7480c6f686a5b29c17094b25ce5d54c816
Mehdi Bekkar <mdz_bondy@hotmail.fr>
2023-10-23 00:26:54 +0200

    index html + portraits photographers

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,38 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+  const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
+  const photographerPageURL = `photographer.html?${id}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const photographerLink = document.createElement("a");
+    photographerLink.setAttribute("href", photographerPageURL);
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const localisation = document.createElement("p");
+    localisation.textContent = city + ", " + country;
+    const accroche = document.createElement("p");
+    accroche.textContent = tagline;
+    const prix = document.createElement("p");
+    prix.textContent = price + "€/jour";
+
+    // Ajouter des classes aux éléments <p>
+    localisation.classList.add("localisation");
+    accroche.classList.add("accroche");
+    prix.classList.add("prix");
+
+    photographerLink.appendChild(img);
+    photographerLink.appendChild(h2);
+    photographerLink.appendChild(localisation);
+    photographerLink.appendChild(accroche);
+    photographerLink.appendChild(prix);
+
+    article.appendChild(photographerLink);
+
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
 }
